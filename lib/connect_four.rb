70009e4f60d10268ebebe6fe1af6317121c8df0b
7minutech <elijohnson1107@gmail.com>
2024-10-30 12:22:53 -0500

    refactor: player_move method added ability to quit
    Should let user be able to quit out of game

--- lib/connect_four.rb
+++ lib/connect_four.rb
@@ -25,8 +25,11 @@ class ConnectFour
   end
 
   def player_input
-    puts "Please enter number from 1-7 to place in a column: "
+    puts "Please enter number from 1-7 to place in a column or q to quit: "
     @move = gets.to_s.chomp
+    return unless @move == "q"
+
+    abort "Exiting game..."
   end
 
   def display_board
