ecab1d0e9befbaa0ebe681e6b5060ea3b8139f1d
7minutech <elijohnson1107@gmail.com>
2024-10-29 20:35:21 -0500

    feat: added what board will look like
    Keeping it for future will delete once board
    method has been created

---
+++ main.rb
@@ -0,0 +1,5 @@
+black_circle = "\u26AB"
+empty_circle = "\u3007"
+white_circle = "\u26AA"
+
+print "[#{white_circle}| #{black_circle}| #{empty_circle}| #{empty_circle}| #{empty_circle}| #{empty_circle}]"
